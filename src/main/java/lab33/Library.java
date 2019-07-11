73ddfb5b56b44301f73e97d544e514889811bb94
xialiu1988 <liuxia198896@gmail.com>
2019-07-11 09:29:50 -0700

    updated

--- src/main/java/lab33/Library.java
+++ src/main/java/lab33/Library.java
@@ -3,8 +3,23 @@
  */
 package lab33;
 
+import java.util.HashSet;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+    public String repeatedWord(String inputWord) {
+
+        String[] wordArray = inputWord.split("\\s*(=>|,|\\s)\\s*");
+        HashSet hashSet = new HashSet();
+
+        for (int i = 0; i < wordArray.length; i++) {
+            if (hashSet.contains(wordArray[i])) {
+                return wordArray[i];
+            } else {
+                hashSet.add(wordArray[i].toLowerCase());
+            }
+        }
+        return null;
     }
+
+
 }
